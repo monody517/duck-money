d7be2c30499732a61455f51eb1382162bd62a200
monody157 <2737716714@qq.com>
2021-05-22 16:27:57 +0800

    deploy

---
+++ precache-manifest.69b1752644531c97af2d681c24b3ea65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2599316dc63602417ec",
+    "url": "/duck-money/css/app.47a081f0.css"
+  },
+  {
+    "revision": "be5e807d28a61fa5976ede3d90700038",
+    "url": "/duck-money/index.html"
+  },
+  {
+    "revision": "b2599316dc63602417ec",
+    "url": "/duck-money/js/app.c7840c56.js"
+  },
+  {
+    "revision": "a0619c06640139a90f96",
+    "url": "/duck-money/js/chunk-vendors.1c6809aa.js"
+  },
+  {
+    "revision": "d1372d61b03359d367fb319fe20d26c9",
+    "url": "/duck-money/manifest.json"
+  },
+  {
+    "revision": "70d70eac69db9fc1b81844cf0505f67e",
+    "url": "/duck-money/qrcode.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/duck-money/robots.txt"
+  }
+]);
